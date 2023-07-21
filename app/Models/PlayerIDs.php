2ee2bd155f97c0635f6991f3e244f808499536ca
Jayden Wang <aleina.wang29@gmail.com>
2023-07-21 00:42:01 -0400

    raging

--- app/Models/PlayerIDs.php
+++ app/Models/PlayerIDs.php
@@ -5,7 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class PlayerIDs extends Model{
     protected $table='playerids';
-    protected $fillable=['id', 'PlayerID', 'firstname', 'lastname', 'team', 'position'];
+    protected $fillable=['PlayerID', 'firstname', 'lastname', 'team', 'position'];
+
+    public $timestamps=false;
 
 }
 
